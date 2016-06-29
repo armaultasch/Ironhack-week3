c546c622c299c019c069fe2b78d431f7e85c8700
Alison Maultasch <armaultasch@gmail.com>
2016-06-29 11:03:23 -0400

    java/ruby compare

--- day3/first_java_ex/file.js
+++ day3/first_java_ex/file.js
@@ -1,3 +1,33 @@
 // file.js
 
-var exampleArray = [];
+
+
+var colorsArray = ["indigo", "black", "indigo", "purple", "indigo", "chartreuse", "indigo", "magenta", "chartreuse", "scarlet"];
+
+function countColors (color, theArray) {
+	var counter = 0;
+	var colorOccurances = 0;
+	for (counter = 0; counter < theArray.length; counter ++) {
+		var colorFromArray = theArray[counter];
+
+		
+		if (colorFromArray === color) {
+			colorOccurances +=1;
+		}
+		//need to do something with color var
+	}
+		
+	return colorOccurances;
+}
+
+console.log("Testing countColors function");
+
+
+
+console.log( countColors("indigo", colorsArray) === 4);
+//ruby equivalent 
+//p countColors("indigo", colors_array) == 2
+console.log( countColors("black", colorsArray) === 1);
+
+console.log( countColors("pink", colorsArray) === 0);
+
